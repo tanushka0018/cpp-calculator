808b692598b8b86296e78487e16a22dce93b3fe2
Tatiana <vsp015@gmail.com>
2025-08-16 22:29:05 -0700

    refactor: add ApplyOperation

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -9,7 +9,7 @@
 #include <string>
 
 
-bool ReadNumber(Number& result) {
+bool ReadNumber(Number &result) {
     if (std::cin >> result) {
         return true;
     }
@@ -22,6 +22,20 @@ bool ReadNumber(Number& result) {
     return false;
 }
 
+bool ApplyOperation(Number &current_value, const std::string &operation) {
+    Number number;
+    if (!ReadNumber(number)) return false;
+
+    if (operation == "+") current_value += number;
+    else if (operation == "-") current_value -= number;
+    else if (operation == "*") current_value *= number;
+    else if (operation == "/") current_value /= number;
+    else /* ** */
+        current_value = std::pow(current_value, number);
+
+    return true;
+}
+
 bool RunCalculatorCycle() {
     Number current_value;
 
@@ -32,56 +46,26 @@ bool RunCalculatorCycle() {
     Number mem_value = 0.0;
     bool has_mem = false;
 
-    for (std::string token; std::cin >> token; ) {
+    for (std::string token; std::cin >> token;) {
         if (token == "q") {
             return true;
         } else if (token == "=") {
             std::cout << current_value << std::endl;
-
         } else if (token == ":") {
-            if (!ReadNumber(current_value)) {
-                return false;
-            }
-
+            if (!ReadNumber(current_value)) return false;
         } else if (token == "c") {
             current_value = 0.0;
-
         } else if (token == "s") {
             has_mem = true;
             mem_value = current_value;
-
         } else if (token == "l") {
             if (!has_mem) {
                 std::cerr << "Error: Memory is empty" << std::endl;
                 return false;
             }
             current_value = mem_value;
-
-        } else if (token == "+") {
-            Number number;
-            if (!ReadNumber(number)) return false;
-            current_value += number;
-
-        } else if (token == "-") {
-            Number number;
-            if (!ReadNumber(number)) return false;
-            current_value -= number;
-
-        } else if (token == "*") {
-            Number number;
-            if (!ReadNumber(number)) return false;
-            current_value *= number;
-
-        } else if (token == "/") {
-            Number number;
-            if (!ReadNumber(number)) return false;
-            current_value /= number;
-
-        } else if (token == "**") {
-            Number number;
-            if (!ReadNumber(number)) return false;
-            current_value = std::pow(current_value, number);
-
+        } else if (token == "+" || token == "-" || token == "*" || token == "/" || token == "**") {
+            if (!ApplyOperation(current_value, token)) return false;
         } else {
             std::cerr << "Error: Unknown token " << token << std::endl;
             return false;
@@ -90,4 +74,3 @@ bool RunCalculatorCycle() {
 
     return true;
 }
-
